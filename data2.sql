1f6f141c62fc027b7df24d8f6cab70e48d4d9ff3
Isha-Nanda08 <143190568+Isha-Nanda08@users.noreply.github.com>
2024-01-27 23:03:06 +0530

    insert data

--- data2.sql
+++ data2.sql
@@ -1,33 +1,4 @@
 
-CREATE TABLE continents (
-    continent_code varchar(20) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
--- creating table of countries
-CREATE TABLE countries (
-    country_code varchar(20) PRIMARY KEY,
-    continent_code varchar(20),
-    name VARCHAR(255) NOT NULL,
-    capital VARCHAR(255),
-    FOREIGN KEY (continent_code) REFERENCES continents(continent_code)
-);
-
--- creating table of states
-CREATE TABLE states (
-    state_id varchar(20) PRIMARY KEY,
-    country_code varchar(20),
-    name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (country_code) REFERENCES countries(country_code)
-);
-
--- creating table of cities
-CREATE TABLE cities (
-    city_id varchar(20) PRIMARY KEY,
-    state_id varchar(20),
-    name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(state_id)
-);
 
 -- afganistan
 
